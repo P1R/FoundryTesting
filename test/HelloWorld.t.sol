3208f88328f171abd77faadd091c47e89d9c78e7
David E. Perez Negron R <david@nethunters.xyz>
2023-08-19 21:44:09 -0600

    Foundry test: introduction

---
+++ test/HelloWorld.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {HelloWorld} from "../src/HelloWorld.sol";
+
+contract CounterTest is Test {
+    HelloWorld public helloWorld;
+
+    function setUp() public {
+        helloWorld = new HelloWorld();
+    }
+
+    function testGreet() public {
+        assertEq(
+            helloWorld.greet(),
+            "Hello World!"
+        );
+    }
+
+}
