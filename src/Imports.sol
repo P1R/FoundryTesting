97d07e9ea7a55e4558937a16428c7b4aadb550b0
David E. Perez Negron R <david@nethunters.xyz>
2023-08-23 23:27:55 -0600

    Remappings

---
+++ src/Imports.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.20;
+
+import "solmate/tokens/ERC20.sol";
+
+contract Token is ERC20("name", "symbol", 18){}
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestOz is Ownable {}
